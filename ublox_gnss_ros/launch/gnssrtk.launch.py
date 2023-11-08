b640a14365bd47541d5ee6a7e1236e2555a6566c
zeki <nguyenhatrung411@gmail.com>
2023-11-08 08:32:47 +0000

    up

--- ublox_gnss_ros/launch/gnssrtk.launch.py
+++ ublox_gnss_ros/launch/gnssrtk.launch.py
@@ -1,17 +1,33 @@
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+
+# def generate_launch_description():
+#     return LaunchDescription([
+#         Node(
+#             package='ublox_gnss',
+#             executable='gnss_pub',
+#             name='gnss_pub',
+#         ),
+
+#         Node(
+#             package='rtcm_provider',
+#             executable='rtcm_ntrip_pub',
+#             name='rtcm_ntrip_pub',
+#         )
+#     ])
+
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(
-            package='ublox_gnss',
-            executable='gnss_pub',
+        ExecuteProcess(
+            cmd=['ros2', 'run', 'ublox_gnss', 'gnss_pub'],
             name='gnss_pub',
         ),
 
-        Node(
-            package='rtcm_provider',
-            executable='rtcm_ntrip_pub',
+        ExecuteProcess(
+            cmd=['ros2', 'run', 'rtcm_provider', 'rtcm_ntrip_pub'],
             name='rtcm_ntrip_pub',
         )
     ])
