8c910ad10db6b791bcce418f116690e1fca29656
zeki <nguyenhatrung411@gmail.com>
2024-01-25 12:51:12 +0000

    up

--- ublox_gnss_ros/setup.py
+++ ublox_gnss_ros/setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
-        (os.path.join('share', package_name), glob('config/*'))
+        (os.path.join('share', package_name, 'config'), glob('config/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
