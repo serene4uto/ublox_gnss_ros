9edc23b24445f8b1d10c8c5a277a405922c12ac1
zeki <nguyenhatrung411@gmail.com>
2024-01-25 12:09:15 +0000

    up

---
+++ ublox_gnss_ros/launch/mapviz.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch import LaunchContext
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    gps_wpf_dir = get_package_share_directory("ublox_gnss_ros")
+    mapviz_config_file = os.path.join(gps_wpf_dir, "config", "gps_wpf_demo.mvc")
+
+    node_mapviz = Node(
+        package="mapviz",
+        executable="mapviz",
+        name="mapviz",
+        parameters=[{"config": mapviz_config_file}]
+    )
+
+
+    ld = LaunchDescription()
+
+    ld.add_action(node_mapviz)
+
+    return ld
